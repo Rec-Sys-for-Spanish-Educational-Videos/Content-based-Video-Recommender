50b53eff00561a23ec91c995ffbed1325644460c
Mickey <gabi_turcu_1997@yahoo.com>
2019-04-02 16:56:59 +0200

    Refactoring, further optimization

--- Source.py
+++ Source.py
@@ -17,7 +17,6 @@ import pandas as pd
 import re
 import tensorflow_hub as hub
 import tensorflow as tf
-from langdetect import detect
 
 def ldaResultsForCluster(cluster):
     for idx, topic in lda_models[cluster].print_topics(-1):
@@ -44,20 +43,21 @@ with open('videos_upv_cleaned.json') as f:
 
 documents = []
 preprocessedDocumentsList = []
+videoIdDictionary = {}
 nrOfTranscriptsToProcess = 45000
 
+nr=0
 for i in range(1,nrOfTranscriptsToProcess):
-    try:
-        if data[i]["transcription"] is not "" and detect(data[i]["transcription"])=='en':
-            preprocessedTranscript = preprocess(data[i]["transcription"])
-            documents.append(preprocessedTranscript)
-            wordList = re.sub("[^\w]", " ",  preprocessedTranscript).split()
-            words = []
-            for word in wordList:
-                words.append(word)
-            preprocessedDocumentsList.append(words)
-    except:
-        print("unable to detect",i)
+    if data[i]["transcription"] is not "" and len(data[i]["transcription"])>6000:
+        preprocessedTranscript = preprocess(data[i]["transcription"])
+        videoIdDictionary[nr]=i
+        nr+=1
+        documents.append(preprocessedTranscript)
+        wordList = re.sub("[^\w]", " ",  preprocessedTranscript).split()
+        words = []
+        for word in wordList:
+            words.append(word)
+        preprocessedDocumentsList.append(words)
 
 #Using word embedding on all the transcripts
 embed = hub.Module("https://tfhub.dev/google/nnlm-es-dim128-with-normalization/1")
@@ -68,65 +68,59 @@ with tf.Session() as session:
     embeddedDocuments = session.run(embed(documents))        
 
 #Clustering
-true_k = 3
-model = KMeans(n_clusters=true_k, init='k-means++', max_iter=300, n_init=3,verbose=True)
+numberOfClusters = 4
+model = KMeans(n_clusters=numberOfClusters, init='k-means++', max_iter=300, n_init=3,verbose=True)
 model.fit(embeddedDocuments)
 
 #Separating the transcript variables according to the cluster they belong into 
 
 preprocessedListForDictionary = []
 
-for i in range(true_k):
+for i in range(numberOfClusters):
     preprocessedListForDictionary.append([])
 
-nr=0
-for i in range(1,nrOfTranscriptsToProcess):
-    if data[i]["transcription"] is not "":
-        prediction = model.predict(embeddedDocuments[nr].reshape(1,-1))
-        preprocessedListForDictionary[prediction[0]].append(preprocessedDocumentsList[nr])
-        nr+=1
+for index, document in enumerate(embeddedDocuments):
+    prediction = model.predict(document.reshape(1,-1))
+    preprocessedListForDictionary[prediction[0]].append(preprocessedDocumentsList[index])
 
-
-clusterWords = [None] * true_k
+# Creating the dictionaries for the LDA models
+        
 dictionary = []
-for i in range(true_k):
+for i in range(numberOfClusters):
     dictionary.append(gensim.corpora.Dictionary(preprocessedListForDictionary[i]))
     dictionary[i].filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
-    clusterWords[i]=[]
     
-lda_models = [None] * true_k
-bow_corpus = [None] * true_k  
-corpus_tfidf = [None] * true_k
-tfidf_models = [None] * true_k
+lda_models = [None] * numberOfClusters
+bow_corpus = [None] * numberOfClusters  
+corpus_tfidf = [None] * numberOfClusters
+tfidf_models = [None] * numberOfClusters
+
+# Creating the LDA models
   
-for i in range(true_k):
+for i in range(numberOfClusters):
     bow_corpus[i] = [dictionary[i].doc2bow(doc) for doc in preprocessedListForDictionary[i]]
     lda_models[i] = gensim.models.LdaMulticore(bow_corpus[i], num_topics=5, id2word=dictionary[i], passes=10, workers=10)
-    print("Cluster ",i)
 
 querryDataFrame = pd.DataFrame(columns=['VideoID','Assigned Cluster','LDA Scores'])
 
-nr=0
-for i in range(1,nrOfTranscriptsToProcess):
-    if data[i]["transcription"] is not "":
-        prediction = model.predict(embeddedDocuments[nr].reshape(1,-1))
-        nr+=1
-        wordList = re.sub("[^\w]", " ",  data[i]["transcription"]).split()
+for index, document in enumerate(embeddedDocuments):
+        prediction = model.predict(document.reshape(1,-1))
+        wordList = re.sub("[^\w]", " ",  documents[index]).split()
         words = []
         for word in wordList:
             words.append(word)
         bow_corpus_predict = dictionary[prediction[0]].doc2bow(words)
-        newLine = pd.DataFrame([[i,prediction[0],lda_models[prediction[0]][bow_corpus_predict]]],
-                               columns=['VideoID','Assigned Cluster','CorrLDA Scores'])
+        newLine = pd.DataFrame([[videoIdDictionary[index],prediction[0],lda_models[prediction[0]][bow_corpus_predict]]],
+                               columns=['VideoID','Assigned Cluster','LDA Scores'])
         querryDataFrame = querryDataFrame.append(newLine,ignore_index = True)
 
 
-querry = 'permanente parala  proteccion  delos  animales  encria instituido'
-Y=[]
+querry = 'gases dióxido de azufre sulfuro de hidrógeno cloro monóxido ydióxido de carbono óxidos nitrosos'
+embeddedQuery=[]
 with tf.Session() as session:
     session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-    Y = session.run(embed([querry]))
-querryCluster = model.predict(Y)[0]
+    embeddedQuery = session.run(embed([querry]))
+querryCluster = model.predict(embeddedQuery)[0]
 wordList = re.sub("[^\w]", " ", querry).split()
 words = []
 for word in wordList:
@@ -142,7 +136,7 @@ scoreDifferences = {}
 for index,entry in querryDataFrame.iterrows():
     if entry['Assigned Cluster'] == querryCluster:
         totalScore=0
-        for score in querryDataFrame.at[index,'CorrLDA Scores']:
+        for score in querryDataFrame.at[index,'LDA Scores']:
             if score[0] in querryScores:
                 totalScore += abs(score[1]-querryScores[score[0]])
             else:
@@ -150,4 +144,10 @@ for index,entry in querryDataFrame.iterrows():
         scoreDifferences[entry['VideoID']]=totalScore
 
 import operator    
-sorted_x = sorted(scoreDifferences.items(), key=operator.itemgetter(1))
+sortedScores = sorted(scoreDifferences.items(), key=operator.itemgetter(1))
+
+
+# Priting the top 5 transcripts with their scores.
+for score in sortedScores[:5]:
+    print(score)
+    print(data[score[0]]["transcription"])
